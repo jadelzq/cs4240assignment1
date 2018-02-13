c18566854a48a09914a5ee129153f90447bd68d6
Jade Lim <miuucheese@gmail.com>
2018-02-13 17:57:53 +0800

    added terrain and dancing mooseman

---
+++ Assignment2/Assets/Script/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    //default unity3D function to detect collision
+    void OnTriggerEnter(Collider other)
+    {
+        //if there is collision
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            //become invisible
+            other.gameObject.SetActive(false);
+        }
+    }
+}
