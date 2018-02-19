fbe647ae078e6411b25059b30d4db1267755cb09
sadhikabilla <svjbilla@gmail.com>
2018-02-19 17:01:43 +0800

    Terrain and script for boat

---
+++ Assignment2/Assets/Script/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject mooseMan;
+	public int maxScore;
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+
+	public void AddScore(){
+		score++;
+		if (score == maxScore) {
+			EndGame ();
+		}
+	}
+
+	void EndGame(){
+		//mooseman dance
+	}
+}
